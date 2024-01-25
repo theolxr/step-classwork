1d5666a1fb586d6884a39d5af83dabd43e1d504a
theolxr <teodorseremet@gmail.com>
2024-01-25 19:32:20 +0200

    Modified Main java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world")
+		System.out.println("Hello world!");
+
+		int x = 20;
+
+		if ( x%2 == 0){
+			System.out.println("Par");
+		}
+		else{
+			System.out.println("Impar");
+		}
 	}
 }
